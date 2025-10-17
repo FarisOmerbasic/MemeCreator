692b86cc36768064c212ffee79556a3e783ceeb2
FarisOmerbasic <farisomerbasic@hotmail.com>
2025-10-17 19:06:13 +0200

    Implemented configuration to save and retrieve data

---
+++ backend/services/configService.js
@@ -0,0 +1,26 @@
+const { v4: uuidv4 } = require('uuid')
+
+const configs = new Map();
+
+function createConfig(payload) {
+    const id = uuidv4();
+    const newConfig = {id, ...payload};
+    configs.set(id, newConfig);
+    return newConfig;
+}
+
+function updateConfig(id, payload) {
+    const existingConfig = configs.get(id);
+    if (!existingConfig) {
+        return null;
+    }
+
+    const updatedConfig = {...existingConfig, ...payload,id};
+    configs.set(id, updateConfig)
+    return updatedConfig;
+}
+
+module.exports = {
+    createConfig,
+    updateConfig,
+};
