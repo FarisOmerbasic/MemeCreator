8917f53e402a7be3f39bdf55e3f11dd16d860a8c
FarisOmerbasic <farisomerbasic@hotmail.com>
2025-10-17 21:23:47 +0200

    Started with the logic

---
+++ backend/services/imageService.js
@@ -0,0 +1,19 @@
+const sharp = require('sharp')
+
+function buildOverlay(width, height, params) {
+    const {
+    topText = '',
+    bottomText = '',
+    fontfamily = 'Impact',
+    fontSize = 64,
+    textColor = 'FFFFFF',
+    strokeColor = '#000000',
+    strokeWidth = 3,
+    textAlign = 'center',
+    padding = 20,
+    allCaps = true,
+    watermarkImage,
+    watermarkPosition = 'bottom-right'
+    } = params
+
+}
