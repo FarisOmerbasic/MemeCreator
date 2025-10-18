86ca59781fc34c02bedbfe645bf4d606b85b8db1
FarisOmerbasic <farisomerbasic@hotmail.com>
2025-10-18 19:33:15 +0200

    Added watermark and scaledown for preview

--- backend/services/imageService.js
+++ backend/services/imageService.js
@@ -1,4 +1,5 @@
-const sharp = require('sharp')
+const sharp = require('sharp');
+const { param } = require('../routes/config');
 
 function buildOverlay(width, height, params) {
     const {
@@ -30,6 +31,27 @@ function buildOverlay(width, height, params) {
     x = width - padding
    }
 
+   let watermarkSvg = '';
+   if (watermarkImage) {
+    const watermarkSize = Math.floor(width * 0.1);
+    let watermarkX = width - watermarkSize - padding;
+    let watermarkY = height - watermarkSize - padding;
+
+    if (watermarkPosition === 'top-left'){
+        watermarkX = padding;
+        watermarkY = padding;
+    } 
+    else if (watermarkPosition === 'top-right') {
+        watermarkX = width - watermarkSize - padding;
+         watermarkY = padding}
+    else if (watermarkPosition === 'bottom-left') {
+        watermarkX = padding;
+        watermarkY = height - watermarkSize - padding;
+    }
+    watermarkSvg = `<image href="${watermarkImage}" x="${watermarkX}" y="${watermarkY}" width="${watermarkSize}" height="${watermarkSize}" />`;
+
+   }
+
    const svg = `
    <svg width="${width}" height="${height}">
    <style>
@@ -45,10 +67,25 @@ function buildOverlay(width, height, params) {
    </style>
    <text x="${x}" y="${padding + fontSize}" class="meme-text" dominant-baseline="hanging">${finalTopText}</text>
    <text x="${x}" y="${height - padding}" class="meme-text" dominant-baseline="auto">${finalBottomText}</text>
+   ${watermarkSvg}
    </svg>`
 
    return Buffer.from(svg);
 }
+
+   async function Preview(imageBuffer, params){
+    const scale = params.scaleDown || 0.05;
+    const image = sharp(imageBuffer);
+    const metadata = await image.metadata();
+    const newWidth = Math.round(metadata.width * scale);
+    const newHeight = Math.round(metadata.height * scale);
+    const textOverlay = buildOverlay(newWidth, newHeight, param)
+    const finalImageBuffer = await sharp(imageBuffer)
+    .resize(newWidth, newHeight)
+    .composite([{input: textOverlay}])
+    .png()
+    .toBuffer();
+   }
 
     async function Generate(imageBuffer, params) {
         const image = sharp(imageBuffer);
