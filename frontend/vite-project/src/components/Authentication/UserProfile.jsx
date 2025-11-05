261677b632488f7452ce5a3bb0b41b56744647b3
FarisOmerbasic <farisomerbasic@hotmail.com>
2025-11-05 21:05:31 +0100

    Formatted authentication files using prettier

--- frontend/vite-project/src/components/Authentication/UserProfile.jsx
+++ frontend/vite-project/src/components/Authentication/UserProfile.jsx
@@ -1,12 +1,12 @@
-import { useAuth0 } from '@auth0/auth0-react'
-import './AuthStyles.css'
+import { useAuth0 } from "@auth0/auth0-react";
+import "./AuthStyles.css";
 
 export default function UserProfile() {
-    const {user, isAuthenticated, isLoading} = useAuth0();
-    if (isLoading || !isAuthenticated) return null;
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  if (isLoading || !isAuthenticated) return null;
   return (
     <div className="profile">
       <span>{user.email}</span>
     </div>
-  )
+  );
 }
